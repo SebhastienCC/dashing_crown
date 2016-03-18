bc84e5e1634734efedc165f140b170d82deeb4bb
Matthew Thompson <matt@mattathompson.org>
2016-03-18 09:29:12 -0400

    More changes

--- jobs/analytics.rb
+++ jobs/analytics.rb
@@ -5,24 +5,28 @@ require 'pry'
 require 'legato'
 require 'oauth2'
 require_relative "../lib/google_token_factory"
-#
-#
-#   def auth_token
-#     client = OAuth2::Client.new(ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], {
-#       :authorize_url => 'https://accounts.google.com/o/oauth2/auth',
-#       :token_url => 'https://accounts.google.com/o/oauth2/token'
-#     })
-#     client.auth_code.authorize_url({
-#       :scope => 'https://www.googleapis.com/auth/analytics.readonly',
-#       :redirect_uri => ENV["DOMAIN"],
-#       :access_type => 'offline'
-#     })
-#   end
-#
-#
-#
-#
-# SCHEDULER.every '30s', :first_in => 0 do |job|
-#   # auth_token
-#   # send_event('rollbar', projects: projects)
-# end
+
+
+  def auth_token
+    client.update!(
+      :additional_parameters => {"access_type" => "offline"}
+    )
+    client = OAuth2::Client.new(ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], {
+      :authorize_url => 'https://accounts.google.com/o/oauth2/auth',
+      :token_url => 'https://accounts.google.com/o/oauth2/token'
+    })
+    client.auth_code.authorize_url({
+      :scope => 'https://www.googleapis.com/auth/analytics.readonly',
+      :redirect_uri => ENV["DOMAIN"],
+      :access_type => 'offline'
+    })
+  end
+
+
+
+
+SCHEDULER.every '30m', :first_in => 0 do |job|
+  binding.pry
+  # auth_token
+  # send_event('convergence', current: projects)
+end
--- jobs/tickets.rb
+++ jobs/tickets.rb
@@ -1,17 +1,21 @@
 require "httparty"
 require "pry"
 
-def filter
+def filter(id)
   auth = { :username => "mthompson", :password => "m200266756" }
-  res  = HTTParty.get "https://crownandcaliber.atlassian.net/rest/api/2/filter/10802", :basic_auth => auth
+  res  = HTTParty.get "https://crownandcaliber.atlassian.net/rest/api/2/filter/#{id}", :basic_auth => auth
   body = HTTParty.get res["searchUrl"], :basic_auth => auth
 end
 
 
 
+11102
 SCHEDULER.every '1m', :first_in => 0 do |job|
+  form_submissions = filter("11102")
+  send_event( "inbox", current: form_submissions.count )
+
   %w(mthompson bcase yliu).each do |username|
-    body = filter
+    body = filter("10802")
     res = body["issues"].select { |x| x["fields"]["assignee"]["name"] == username }
     send_event( username, current: res.count )
   end
