aa6fe1816601a0fbc545b187b1faa149e729fe0f
Matthew Thompson <matt@mattathompson.org>
2016-03-21 20:52:23 -0400

    Update the analytics

--- jobs/analytics.rb
+++ jobs/analytics.rb
@@ -1,25 +1,6 @@
-require 'httparty'
 require 'dotenv'
 Dotenv.load
 require 'google/api_client'
-
-
-  def auth_token
-    client.update!(
-      :additional_parameters => {"access_type" => "offline"}
-    )
-    client = OAuth2::Client.new(ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], {
-      :authorize_url => 'https://accounts.google.com/o/oauth2/auth',
-      :token_url => 'https://accounts.google.com/o/oauth2/token'
-    })
-    client.auth_code.authorize_url({
-      :scope => 'https://www.googleapis.com/auth/analytics.readonly',
-      :redirect_uri => ENV["DOMAIN"],
-      :access_type => 'offline'
-    })
-  end
-
-
 
 
 SCHEDULER.every '10s', :first_in => 0 do |job|
