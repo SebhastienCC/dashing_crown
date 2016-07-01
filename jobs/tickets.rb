636b4f01a5ed98756fdefe6db0c9222919264b90
Eunice Choi <euniceschoi93@gmail.com>
2016-07-01 13:53:02 -0400

    change filter

--- jobs/tickets.rb
+++ jobs/tickets.rb
@@ -2,7 +2,7 @@ require "httparty"
 require "pry"
 
 def filter(id)
-  auth = { :username => "echoi", :password => "iloveteddie93" }
+  auth = { :username => "echoi", :password => "CrownCaliber123" }
   res  = HTTParty.get "https://crownandcaliber.atlassian.net/rest/api/2/filter/#{id}", :basic_auth => auth
   body = HTTParty.get "#{res["searchUrl"]}&maxResults=500", :basic_auth => auth
 end
@@ -10,7 +10,7 @@ end
 
 
 SCHEDULER.every '1m', :first_in => 0 do |job|
-  form_submissions = filter("10620")
+  form_submissions = filter("12502")
   send_event( "inbox", current: form_submissions["issues"].count )
   body = filter("12502")
 
