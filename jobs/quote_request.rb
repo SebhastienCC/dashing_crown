a4834a8f88a04cbd4b17524c7cf0b8a6a6d5e1a6
Brandon Case <bcase@crownandcaliber.com>
2016-08-22 13:07:30 -0400

    added unprocessed qrs to the view
    Signed-off-by: Brandon Case <bcase@crownandcaliber.com>

--- jobs/quote_request.rb
+++ jobs/quote_request.rb
@@ -10,4 +10,5 @@ end
 SCHEDULER.every '5m', :first_in => 0 do |job|
   response = HTTParty.get "https://#{ENV['CROWN_DOMAIN']}/v1/quote_requests?authentication_token=#{authenticate}"
   send_event('quote_request', current: response["quote_requests"].length)
+  send_event('quote_request', currentUnprocessed: response["unprocessed_quote_requests"].length)
 end
